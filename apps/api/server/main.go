b06098212c87986be1a2c3c5be2c49d61c9a625e
Connor Dear <connordear@gmail.com>
2025-04-26 13:18:55 -0600

    pgadmin now loads automatically, server is connected to camps db

--- apps/api/server/main.go
+++ apps/api/server/main.go
@@ -1,33 +1,62 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
-)
+	"server/middleware"
 
-type HealthData struct {
-	Timestamp int `json:"timestamp"`
-}
+	"github.com/jackc/pgx/v5"
+)
 
 var PORT = ":" + string(os.Getenv("SERVER_PORT"))
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	log.Println("HealthChecking!")
-	b, err := json.Marshal(HealthData{Timestamp: 2})
+type Camp struct {
+	Name string `json:"name"`
+}
+
+func getCamps(w http.ResponseWriter, r *http.Request) {
+	var camps []Camp
+
+	cnn, err := connect()
 	if err != nil {
-		log.Fatalf("Unable to marshal to to %s\n", err)
+		log.Fatal(err)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(b)
+
+	rows, cnnErr := cnn.Query(context.Background(), "SELECT name FROM camps")
+	if cnnErr != nil {
+		log.Fatal(cnnErr)
+	}
+
+	camps, mapErr := pgx.CollectRows(rows, pgx.RowToStructByName[Camp])
+	if mapErr != nil {
+		log.Fatal(mapErr)
+	}
+
+	campJson, jsonErr := json.Marshal(camps)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	w.Write(campJson)
+
+	defer rows.Close()
+	defer cnn.Close(context.Background())
 }
 
 func main() {
 	runMigrations()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /health-check", healthCheck)
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /camps", getCamps)
+
+	server := http.Server{
+		Addr:    PORT,
+		Handler: middleware.Logging(router),
+	}
+
 	log.Println("Listening on port", PORT)
-	http.ListenAndServe(PORT, mux)
+	server.ListenAndServe()
 }
--- db/init.sql
+++ db/init.sql
@@ -5,3 +5,10 @@ CREATE TABLE IF NOT EXISTS version (
 
 INSERT INTO version (current_version) VALUES ('0.0.0');
 
+CREATE TABLE IF NOT EXISTS camps (
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name string NOT NULL
+);
+
+INSERT INTO camps (name) VALUES ('New Camp');
+
